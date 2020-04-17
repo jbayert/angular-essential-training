a4ab336b4df8fa5c8105a37d712c639bcbb25931
jbayert <35811092+jbayert@users.noreply.github.com>
2020-04-16 21:16:35 -0500

    my work done on this project

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
   templateUrl: './media-item.component.html',
   styleUrls: ['./media-item.component.css']
 })
-export class MediaItemComponent {}
+
+export class MediaItemComponent {
+  @Input() mediaItem;
+  @Output() delete = new EventEmitter();
+
+  onDelete(){
+    console.log("Test");
+    this.delete.emit(this.mediaItem);
+  }
+  name = "Test"
+  wasWatched(){
+    return true;
+  }
+}
